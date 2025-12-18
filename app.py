a30cf0f5dce013c07ad676899dc23f285b006811
Rishika-Buzunoor <156920015+Rishika-Buzunoor@users.noreply.github.com>
2025-12-18 13:45:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 import pickle
 
 #load the trained model
-model=tf.keras.models.load_model('model.keras')
+model=tf.keras.models.load_model('model.h5')
 
 #load the encoders and scaler
 with open('label_encoder_gender.pkl','rb') as file:
